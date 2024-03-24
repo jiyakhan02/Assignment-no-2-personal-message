0388dad7928a285da8ce83d15e09e466ad2b4e77
JaweriyaMursleen <160059863+jiyakhan02@users.noreply.github.com>
2024-03-24 04:03:30 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-"use strict";
 //Personal Message: Store a person’s name in a variable, and print a message to that person.
 // Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 let personName = "Eric";
